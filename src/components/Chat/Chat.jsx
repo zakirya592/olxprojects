056f3521fda8e8f414bf6c74ad6b6b67e5eb5be4
zakirya592 <muhammadzakirya592@gmail.com>
2024-09-29 11:36:40 +0500

    updated

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,6 +3,7 @@ import NewRequest from "../../../utils/NewRequest";
 import { useQuery } from "react-query";
 import { GiPlayButton } from "react-icons/gi";
 import imageLiveUrl from "../../../utils/urlConverter/imageLiveUrl";
+import { FaSearch } from "react-icons/fa";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
@@ -12,7 +13,6 @@ const Chat = () => {
   const storedUserResponseString = sessionStorage.getItem("userResponse");
   const storedUserResponse = JSON.parse(storedUserResponseString);
   const loginuserdata = storedUserResponse?.data?.user || "";
-  const googleid = storedUserResponse;
 
   const chatproduct = sessionStorage.getItem("chardata");
   const chatResponse = JSON.parse(chatproduct);
@@ -44,8 +44,6 @@ const Chat = () => {
     try {
       const response = await NewRequest.get(`/chat/getmychat?userId=${senderId || ""}`
       );
-      console.log(response, "response");
-
       setchatlist(response.data);
     } catch (error) {
       console.error("Error fetching chat history:", error);
@@ -74,7 +72,6 @@ const Chat = () => {
 
   const handleChatSelection = (chat) => {
     setSelectedUser(chat?.user);
-    console.log(chat?.user);
     setActiveChatId(chat?.user._id);
 
   };
@@ -107,172 +104,262 @@ const Chat = () => {
     }
   }, [chatHistorydata]);
 
+  
+    const [imageshow, setimageshow] = useState("");
+    const [name, setname] = useState("");
+    const [aboutMe, setaboutMe] = useState("");
+
+  // User profile data
+    useEffect(() => {
+      NewRequest.get(`/users/${senderId || ""}`)
+        .then((response) => {
+          const userdata = response.data;
+          const imageUrl = userdata?.image || "";
+          const finalUrl = imageUrl && imageUrl.startsWith("https") ? imageUrl: imageLiveUrl(imageUrl);
+          setimageshow(finalUrl || "");
+          setname(userdata?.username || "");
+          setaboutMe(userdata?.aboutMe || "");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+
+
+     const [searchQuery, setSearchQuery] = useState("");
+
+  // Filtered chatlist based on the search query
+  const filteredChatList = chatlist.filter(
+    (chat) =>
+      chat?.user?.username?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      chat?.lastMessage?.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
 
   return (
-    <div className="flex flex-col sm:flex-col lg:flex-row h-[80vh] overflow-y-scroll lg:px-10 mt-5 lg:mt-40 sm:mt-2">
-      {/* Sidebar */}
-      <div className="w-full lg:w-1/4 sm:w-full  border-gray-300 rounded-md shadow-lg ">
-        <h2 className="text-xl font-semibold mb-4 p-3">Inbox</h2>
-        <div className="space-y-4 p-4">
-          {chatlist.map((chatlist, index) => (
-            <div
-              className={`flex items-center p-2 rounded cursor-pointer shadow ${
-                activeChatId === chatlist?.user._id
-                  ? "bg-gray-300"
-                  : "bg-gray-100"
-              }`}
-              key={index}
-              onClick={() => handleChatSelection(chatlist)}
-            >
-             
-              <img
-                // src={chatlist?.user?.image || ""}
-                src={
-                  chatlist?.user?.image
-                    ? chatlist?.user?.image.startsWith("https")
-                      ? chatlist?.user?.image
-                      : imageLiveUrl(chatlist.user.image)
-                    : ""
-                }
-                alt="User"
-                className="w-10 h-10 rounded-full mr-3"
-              />
-              <div className="w-full">
-                <div className="flex justify-between w-full">
-                  <h3 className="font-medium">
-                    {" "}
-                    {chatlist?.user.username || ""}
-                  </h3>
-                  <span className="text-sm font-semibold text-blue-600">
-                    {/* {chatlist?.timestamp || ""} */}
-                    {new Date(chatlist?.timestamp).toLocaleString("en-US", {
-                      // day: "2-digit",
-                      // month: "2-digit",
-                      // year: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      second: "2-digit",
-                      hour12: true,
-                    })}
-                  </span>
-                </div>
-                <p className="text-gray-500 text-sm">
-                  {chatlist?.lastMessage || ""}
-                </p>
-              </div>
+    <>
+      <div className="flex flex-col sm:flex-col lg:flex-row h-[80vh] overflow-y-scroll lg:px-10 mt-5 lg:mt-28 sm:mt-2">
+        {/* Sidebar */}
+        <div className="w-full lg:w-1/4 sm:w-full h-auto  border-gray-300  rounded-md shadow-lg ">
+          <div className="flex items-center bg-gray-300 p-4 rounded-sm">
+            <img
+              src={imageshow || ""}
+              alt="Avatar"
+              className="w-10 h-10 rounded-full"
+            />
+            {/* Username and Message */}
+            <div className="ml-4">
+              <h3 className="font-bold text-gray-900">{name || ""}</h3>
+              <p className="text-sm text-gray-600 truncate w-40">{aboutMe}</p>
             </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Main Chat Area */}
-      <div className="flex-1 p-5">
-        <div className="border border-gray-300 bg-[#FFFFFF] p-4 h-full flex flex-col justify-between w-full">
-          <div
-            className=" overflow-y-auto mb-5 flex-grow"
-            ref={chatContainerRef}
-          >
-            {chatHistorydata &&
-              chatHistorydata.map((chat, index) => (
-                <div
-                  key={index}
-                  className={`mb-3 ${
-                    chat.sender._id === senderId
-                      ? "text-right mx-4"
-                      : "text-left mx-4"
-                  }`}
-                >
-                  <div>
-                    {/* {chat.sender} */}
-                    <div
-                      className={`flex ${
-                        chat.sender._id === senderId
-                          ? "justify-end"
-                          : "justify-start"
-                      }`}
-                    >
-                      {/* Avatar for the sender */}
-                      {chat.sender._id !== senderId && (
-                        <img
-                          src={
-                            chat.sender.image
-                              ? chat.sender.image.startsWith("https")
-                                ? chat.sender.image
-                                : imageLiveUrl(chat.sender.image)
-                              : ""
-                          }
-                          // src={chat.sender.image ? (chat.sender.image.startsWith("https") ? chat.sender.image : imageLiveUrl(chat.sender.image)) : ""}
-                          alt={chat.sender.name}
-                          className="w-8 h-8 rounded-full mr-2"
-                        />
-                      )}
-
-                      <div
-                        className={`${
-                          chat.sender._id === senderId
-                            ? "bg-[#F5F7FB] text-black"
-                            : " bg-indigo-600 text-white py-2 px-4 rounded-lg max-w-xs shadow-lg"
-                        } py-2 px-4 rounded-lg max-w-xs shadow-lg`}
-                      >
-                        <div className="flex">
-                          <p className=""> {chat?.content || ""}</p>
-                        </div>
-                        <p className="text-xs text-gray-400">
-                          {new Date(chat.timestamp).toLocaleString("en-US", {
-                            // day: "2-digit",
-                            // month: "2-digit",
-                            // year: "numeric",
-                            hour: "2-digit",
-                            minute: "2-digit",
-                            second: "2-digit",
-                            hour12: true,
-                          })}
-                        </p>
-                      </div>
-
-                      {/* Show the current user's image if they are the sender */}
-                      {chat.sender._id === senderId && (
-                        <img
-                          // src={chat.sender.image} // URL for the current user's image
-                          src={
-                            chat.sender.image
-                              ? chat.sender.image.startsWith("https")
-                                ? chat.sender.image
-                                : imageLiveUrl(chat.sender.image)
-                              : ""
-                          }
-                          alt={chat.sender.name} // Current user's name
-                          className="w-8 h-8 rounded-full ml-2"
-                        />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              ))}
           </div>
+          <div className="text-xl font-semibold p-3">
+            <div className="relative">
+              {/* Search Icon */}
+              <span className="absolute inset-y-0 left-3 flex items-center text-gray-400">
+                <FaSearch />
+              </span>
 
-          <div className="p-4 border-t border-gray-300">
-            <div className="flex">
+              {/* Input Field */}
               <input
                 type="text"
-                value={message}
-                onChange={handleMessageChange}
-                className="flex-1 px-2 py-4 border border-gray-300 rounded focus:outline-none bg-[#E6EBF5] focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your message..."
-                onKeyDown={handleKeyDown}
+                className="w-full p-2 pl-10 border rounded-lg focus:outline-none "
+                placeholder="Search..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
               />
-              <button
-                onClick={handleSendMessage}
-                className="ml-2 px-4 bg-[#7269EF] text-white rounded hover:bg-blue-600 transition duration-300"
+            </div>
+          </div>
+          <div className="space-y-4 p-4">
+            {filteredChatList.length > 0 ? (
+              filteredChatList.map((chatlist, index) => (
+                <div
+                  className={`flex items-center p-2 rounded cursor-pointer shadow ${
+                    activeChatId === chatlist?.user._id
+                      ? "bg-gray-300"
+                      : "bg-gray-100"
+                  }`}
+                  key={index}
+                  onClick={() => handleChatSelection(chatlist)}
+                >
+                  <img
+                    // src={chatlist?.user?.image || ""}
+                    src={
+                      chatlist?.user?.image
+                        ? chatlist?.user?.image.startsWith("https")
+                          ? chatlist?.user?.image
+                          : imageLiveUrl(chatlist.user.image)
+                        : ""
+                    }
+                    alt="User"
+                    className="w-10 h-10 rounded-full mr-3"
+                  />
+                  <div className="w-full">
+                    <div className="flex justify-between w-full">
+                      <h3 className="font-medium">
+                        {" "}
+                        {chatlist?.user.username || ""}
+                      </h3>
+                      <span className="text-sm font-semibold text-blue-600">
+                        {/* {chatlist?.timestamp || ""} */}
+                        {new Date(chatlist?.timestamp).toLocaleString("en-US", {
+                          // day: "2-digit",
+                          // month: "2-digit",
+                          // year: "numeric",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          second: "2-digit",
+                          hour12: true,
+                        })}
+                      </span>
+                    </div>
+                    <p className="text-gray-500 text-sm">
+                      {chatlist?.lastMessage || ""}
+                    </p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="text-center text-gray-500">No chats found</p>
+            )}
+          </div>
+        </div>
+
+        {/* Main Chat Area */}
+        <div className="flex-1">
+          <div className="flex items-center bg-gray-300 p-4 rounded-sm ms-1">
+            <img
+              // src={imageshow || ""}
+              src={
+                selectedUser.image
+                  ? selectedUser.image.startsWith("https")
+                    ? selectedUser.image
+                    : imageLiveUrl(selectedUser.image)
+                  : ""
+              }
+              alt="Avatar"
+              className="w-10 h-10 rounded-full"
+            />
+            {/* Username and Message */}
+            <div className="ml-4">
+              <h3 className="font-bold text-gray-900">
+                {selectedUser?.username || ""}
+              </h3>
+              {/* <p className="text-sm text-gray-600 truncate w-40">{aboutMe}</p> */}
+            </div>
+          </div>
+          <div className="p-1 h-screen sm:h-screen lg:h-[650px]">
+            <div className="border border-gray-300 bg-[#FFFFFF] p-4 h-full flex flex-col justify-between w-full">
+              <div
+                className=" overflow-y-auto mb-5 flex-grow"
+                ref={chatContainerRef}
               >
-                {/* Send */}
-                <GiPlayButton size={34} />
-              </button>
+                {chatHistorydata &&
+                  chatHistorydata.map((chat, index) => (
+                    <div
+                      key={index}
+                      className={`mb-3 ${
+                        chat.sender._id === senderId
+                          ? "text-right mx-4"
+                          : "text-left mx-4"
+                      }`}
+                    >
+                      <div>
+                        {/* {chat.sender} */}
+                        <div
+                          className={`flex ${
+                            chat.sender._id === senderId
+                              ? "justify-end"
+                              : "justify-start"
+                          }`}
+                        >
+                          {/* Avatar for the sender */}
+                          {chat.sender._id !== senderId && (
+                            <img
+                              src={
+                                chat.sender.image
+                                  ? chat.sender.image.startsWith("https")
+                                    ? chat.sender.image
+                                    : imageLiveUrl(chat.sender.image)
+                                  : ""
+                              }
+                              // src={chat.sender.image ? (chat.sender.image.startsWith("https") ? chat.sender.image : imageLiveUrl(chat.sender.image)) : ""}
+                              alt={chat.sender.name}
+                              className="w-8 h-8 rounded-full mr-2"
+                            />
+                          )}
+
+                          <div
+                            className={`${
+                              chat.sender._id === senderId
+                                ? "bg-[#F5F7FB] text-black"
+                                : " bg-indigo-600 text-white py-2 px-4 rounded-lg max-w-xs shadow-lg"
+                            } py-2 px-4 rounded-lg max-w-xs shadow-lg`}
+                          >
+                            <div className="flex">
+                              <p className=""> {chat?.content || ""}</p>
+                            </div>
+                            <p className="text-xs text-gray-400">
+                              {new Date(chat.timestamp).toLocaleString(
+                                "en-US",
+                                {
+                                  // day: "2-digit",
+                                  // month: "2-digit",
+                                  // year: "numeric",
+                                  hour: "2-digit",
+                                  minute: "2-digit",
+                                  second: "2-digit",
+                                  hour12: true,
+                                }
+                              )}
+                            </p>
+                          </div>
+
+                          {/* Show the current user's image if they are the sender */}
+                          {chat.sender._id === senderId && (
+                            <img
+                              // src={chat.sender.image} // URL for the current user's image
+                              src={
+                                chat.sender.image
+                                  ? chat.sender.image.startsWith("https")
+                                    ? chat.sender.image
+                                    : imageLiveUrl(chat.sender.image)
+                                  : ""
+                              }
+                              alt={chat.sender.name} // Current user's name
+                              className="w-8 h-8 rounded-full ml-2"
+                            />
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+
+              <div className="p-4 border-t border-gray-300">
+                <div className="flex">
+                  <input
+                    type="text"
+                    value={message}
+                    onChange={handleMessageChange}
+                    className="flex-1 px-2 py-4 border border-gray-300 rounded focus:outline-none bg-[#E6EBF5] focus:ring-2 focus:ring-blue-500"
+                    placeholder="Enter your message..."
+                    onKeyDown={handleKeyDown}
+                  />
+                  <button
+                    onClick={handleSendMessage}
+                    className="ml-2 px-4 bg-[#7269EF] text-white rounded hover:bg-blue-600 transition duration-300"
+                  >
+                    {/* Send */}
+                    <GiPlayButton size={34} />
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
