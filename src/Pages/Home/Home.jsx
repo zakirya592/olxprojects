9f03d442ba123c300f10bb7512427c491ddef8a9
zakirya592 <muhammadzakirya592@gmail.com>
2024-10-07 12:42:42 +0500

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import DropDownSelection from '../../components/DropDownSelection/DropDownSelect
 function Home() {
   return (
     <div>
-      <div className="w-full lg:w-[90%]  sm:w-full mx-auto mt-4 lg:mt-40 sm:mt-2 px-4">
+      <div className="w-full lg:w-[90%]  sm:w-full mx-auto mt-4 lg:mt-40 sm:mt-2 lg:px-4 sm:px-1 px-1">
         {/* <DropDownSelection /> */}
         <Hadersilder />
         {/* <Categories /> */}
